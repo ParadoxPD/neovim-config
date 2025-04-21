fcc90d17baeaf5c1fd441f0414417a300f1bdd09
ParadoxPD <ask.priyanshu@gmail.com>
2025-04-21 13:40:59 +0530

    Added markdown preview and rendering

---
+++ lua/custom/plugins/markdown-stuff.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'ParadoxPD/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && npm install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      render_modes = { 'n', 'c', 't' },
+    },
+  },
+}
