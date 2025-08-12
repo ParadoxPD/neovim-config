8ff5c6529ad489b344442503cd3aec0241531252
ParadoxPD <ask.priyanshu@gmail.com>
2025-08-13 01:28:02 +0530

    Edited some keymaps and stuff

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -340,24 +340,7 @@ return {
           },
         },
       },
-      on_attach = function(client, bufnr)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-        -- Key mappings
-        local bufopts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-        vim.keymap.set('n', '<space>f', function()
-          vim.lsp.buf.format { async = true }
-        end, bufopts)
-      end,
+      on_attach = function(client, bufnr) end,
     }
   end,
 }
